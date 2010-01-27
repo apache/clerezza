24df32dff897d13bbebe083a6c7482f2467b69d5
Reto Bachmann-Gmür <reto@apache.org>
2010-01-27 13:37:57 +0000

    CLEREZZA-70: closed
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk/org.apache.clerezza.parent@903636 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.clerezza.webapp.parent/org.apache.clerezza.platform.content/src/main/resources/org/apache/clerezza/platform/content/staticweb/scripts/tria-disco.js
+++ org.apache.clerezza.webapp.parent/org.apache.clerezza.platform.content/src/main/resources/org/apache/clerezza/platform/content/staticweb/scripts/tria-disco.js
@@ -5,15 +5,20 @@ function TriaDisco() {
 
 TriaDisco.putData = function(rdfSymbol, store, previousStore, noContainerCreation) {
     var xhr = Util.XMLHTTPFactory();
-    var postUrl = "post?resource="+rdfSymbol.uri;
+    var postUrl = "post";
 	if (TriaDisco.graphUri != null) {
-		postUrl += "&graph="+TriaDisco.graphUri
+		postUrl += "?graph="+TriaDisco.graphUri
 	}
     xhr.open('POST', postUrl, false);
-    xhr.setRequestHeader("Content-Type", "application/rdf+xml");
-    var assertedRDF = new XMLSerializer().serializeToString(RDFXMLSerializer.serialize(store, ""));
-    xhr.setRequestHeader("Content-length", assertedRDF.length);
-    xhr.send(assertedRDF);
+
+	var assertedRDF = new XMLSerializer().serializeToString(RDFXMLSerializer.serialize(store, ""));
+	var revokedRDF = new XMLSerializer().serializeToString(RDFXMLSerializer.serialize(previousStore, ""));
+	var parameters = "assert="+encodeURIComponent(assertedRDF);
+	parameters += "&revoke="+encodeURIComponent(revokedRDF);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.setRequestHeader("Content-length", parameters.length);
+	//xhr.setRequestHeader("Connection", "close");
+	xhr.send(parameters);
     if (xhr.status >= 300) {
         alert(xhr.status+" " +xhr.statusText);
         throw new Error(xhr.status+" " +xhr.statusText);
