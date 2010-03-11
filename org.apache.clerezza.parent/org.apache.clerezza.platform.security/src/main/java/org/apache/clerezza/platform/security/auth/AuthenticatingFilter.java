be2edd0f88ac8d140c91ac1519888a512eed99b3
Manuel Innerhofer <mir@apache.org>
2010-03-11 17:32:31 +0000

    CLEREZZA-153: removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk@921954 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.clerezza.parent/org.apache.clerezza.platform.security/src/main/java/org/apache/clerezza/platform/security/auth/AuthenticatingFilter.java
+++ org.apache.clerezza.parent/org.apache.clerezza.platform.security/src/main/java/org/apache/clerezza/platform/security/auth/AuthenticatingFilter.java
@@ -20,7 +20,6 @@ package org.apache.clerezza.platform.security.auth;
 
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
-import java.util.Collections;
 
 import java.util.Comparator;
 import java.util.Iterator;
