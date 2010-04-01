1ad7deef96a5ad8331842d9f0bf92a4e0281d4b0
Reto Bachmann-Gmür <reto@apache.org>
2010-04-01 16:43:26 +0000

    CLEREZZA-183 TcPermission: document actions, introduce constants for actions, avoid string operation in actionsImplies-method
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk/org.apache.clerezza.parent@930036 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.clerezza.rdf.core/src/main/java/org/apache/clerezza/rdf/core/access/security/TcPermission.java
+++ org.apache.clerezza.rdf.core/src/main/java/org/apache/clerezza/rdf/core/access/security/TcPermission.java
@@ -19,6 +19,10 @@
 package org.apache.clerezza.rdf.core.access.security;
 
 import java.security.Permission;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 /**
  * A permission to access <code>TripleCollection<code>s matching a specified 
@@ -30,13 +34,41 @@ import java.security.Permission;
  * @author reto, tsuy
  */
 public class TcPermission extends Permission {
-	private String tcNamePattern;
-	private String actions;
+	public final static String READWRITE = "readwrite";
+	public final static String READ = "read";
 
+	private String tcNamePattern;
+	/**
+	 * true if readwrite granted false if only read
+	 */
+	private boolean allowReadWrite = false;
+
+	final static Pattern actionPattern = Pattern.compile(",( *)");
+	/**
+	 * Conststructs a TcPermission for a specified name pattern and a list of
+	 * actions.
+	 *
+	 * @param tcNamePattern see class description
+	 * @param actions a comma separated list of the strings "read" and "readwrite",
+	 *        the canonical form is just "read" or "readwrite" as "readwrite"
+	 *        implies "read".
+	 */
 	public TcPermission(String tcNamePattern, String actions)  {
 		super(tcNamePattern);
 		this.tcNamePattern = tcNamePattern;
-		this.actions = actions;
+		//check and set actions
+		final Set actionSet = new HashSet(Arrays.asList(actionPattern.split(actions)));
+		if (actionSet.remove(READWRITE)) {
+			allowReadWrite = true;
+		} else {
+			if (!actionSet.contains(READ)) {
+				throw new IllegalArgumentException("actions must be either \"read\" or \"readwrite\"");
+			}
+		}
+		actionSet.remove(READ);
+		if (actionSet.size() > 0) {
+			throw new IllegalArgumentException("actions must only contain \"read\" and \"readwrite\"");
+		}
 	}
 
 	@Override
@@ -46,7 +78,7 @@ public class TcPermission extends Permission {
 			if (!patternImplies(other.tcNamePattern)) {
 				return false;
 			}
-			if (!actionsImplies(other.actions)) {
+			if (!actionsImplies(other.allowReadWrite)) {
 				return false;
 			}
 			return true;
@@ -56,11 +88,11 @@ public class TcPermission extends Permission {
 		return false;
 	}
 
-	private boolean actionsImplies(String actionsOther) {
-		if (actionsOther.equals("read")) {
+	private boolean actionsImplies(boolean readwriteOther) {
+		if (!readwriteOther) {
 			return true;
 		} else {
-			return actions.contains("write");
+			return allowReadWrite;
 		}
 		
 	}
@@ -90,9 +122,7 @@ public class TcPermission extends Permission {
 				|| !this.tcNamePattern.equals(other.tcNamePattern))) {
 			return false;
 		}
-		if (this.actions != other.actions 
-				&& (this.actions == null
-				|| !this.actions.equals(other.actions))) {
+		if (this.allowReadWrite != other.allowReadWrite) {
 			return false;
 		}
 		return true;
@@ -103,6 +133,9 @@ public class TcPermission extends Permission {
 		int hash = 3;
 		hash = 97 * hash + (this.tcNamePattern != null ?
 			this.tcNamePattern.hashCode() : 0);
+		if (allowReadWrite) {
+			hash++;
+		}
 		return hash;
 	}
 
@@ -110,7 +143,7 @@ public class TcPermission extends Permission {
 
 	@Override
 	public String getActions() {
-		return this.actions;
+		return allowReadWrite ? READWRITE : READ;
 	}
 
 }
