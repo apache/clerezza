54f01e37d664f63cbd2a035d81974424eecc30b2
Manuel Innerhofer <mir@apache.org>
2010-06-02 14:13:40 +0000

    added additional com.sun packages to 'org.osgi.framework.system.packages.extra'
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk/org.apache.clerezza.parent@950571 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.clerezza.platform.launcher.storageless.parent/org.apache.clerezza.platform.launcher.storageless/src/main/java/org/apache/clerezza/platform/launcher/Main.java
+++ org.apache.clerezza.platform.launcher.storageless.parent/org.apache.clerezza.platform.launcher.storageless/src/main/java/org/apache/clerezza/platform/launcher/Main.java
@@ -254,9 +254,12 @@ public class Main implements BundleActivator {
 			configProps.put("org.osgi.service.http.port", port);
 		}
 		configProps.put("org.osgi.framework.system.packages.extra",
-				"sun.rmi.server;sun.rmi.transport;sun.rmi.transport.tcp;" +
-				"sun.rmi.registry;com.sun.jndi.rmi.registry;sun.io;" +
-				"com.sun.jmx;sun.misc;");
+				"sun.rmi.server;sun.rmi.transport;sun.rmi.transport.tcp;"
+						+ "sun.rmi.registry;com.sun.jndi.rmi.registry;"
+						+ "sun.io;"
+						+ "com.sun.jmx;" 
+						+ "sun.misc;"
+						+ "com.sun.net.ssl.internal.ssl;com.sun.net.ssl");
 		configProps.put("org.ops4j.pax.url.mvn.repositories", getCommaSeparatedListOfMavenRepos());
 
 
