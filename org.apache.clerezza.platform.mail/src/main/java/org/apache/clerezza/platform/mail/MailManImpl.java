5dee26c150e80cd3935cc9eceb6d1858f798ab8c
Reto Bachmann-Gmür <reto@apache.org>
2010-06-04 11:43:47 +0000

    CLEREZZA-36: extracting SesseionFactory, deprecated methods
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk/org.apache.clerezza.parent@951382 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.clerezza.platform.mail/src/main/java/org/apache/clerezza/platform/mail/MailManImpl.java
+++ org.apache.clerezza.platform.mail/src/main/java/org/apache/clerezza/platform/mail/MailManImpl.java
@@ -24,19 +24,15 @@ import java.security.PrivilegedExceptionAction;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.Properties;
 
-import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.ws.rs.core.MediaType;
-import org.osgi.service.component.ComponentContext;
 import org.apache.clerezza.platform.typerendering.Renderer;
 import org.apache.clerezza.platform.typerendering.RendererFactory;
 import org.apache.clerezza.rdf.core.MGraph;
@@ -50,81 +46,34 @@ import org.apache.clerezza.rdf.core.sparql.query.SelectQuery;
 import org.apache.clerezza.rdf.ontologies.FOAF;
 import org.apache.clerezza.rdf.ontologies.PLATFORM;
 import org.apache.clerezza.rdf.utils.GraphNode;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
 
 /**
- * @scr.component
- * @scr.service interface="org.apache.clerezza.platform.mail.MailMan"
+ *
+ * Implementation of <code>MailMan</code> providing  utility methods to send
+ * emails rendering GraphNodes
  *
  * @author mir, daniel
  */
+@Component
+@Service(MailMan.class)
+@Reference
 public class MailManImpl implements MailMan {
 
-	/**
-	 * Service property
-	 *
-	 * @scr.property value="false"
-	 *               description="Specifies if TLS (SSL) encryption is used."
-	 */
-	public static final String MAIL_USE_TLS = "useTLS";
-	/**
-	 * Service property
-	 *
-	 * @scr.property value="smtp.myHost.org"
-	 *               description="Specifies the SMTP host."
-	 */
-	public static final String MAIL_SMTP_HOST = "smtpHost";
-	/**
-	 * Service property
-	 *
-	 * @scr.property value="25" description="Specifies the SMTP port."
-	 */
-	public static final String MAIL_SMTP_PORT = "smtpPort";
-	/**
-	 * Service property
-	 *
-	 * @scr.property value="myPassword" description=
-	 *               "Specifies the authentication password (plain text) for SMTP."
-	 */
-	public static final String MAIL_SMTP_PASSWORD = "smtpPassword";
-	/**
-	 * Service property
-	 *
-	 * @scr.property value="myUserName"
-	 *               description="Specifies the User to authenticate for SMTP."
-	 */
-	public static final String MAIL_SMTP_USER = "smtpUser";
-	private Properties properties;
-	/**
-	 * @scr.reference
-	 */
+	@Reference
 	private TcManager tcManager;
 
-	/**
-	 * @scr.reference
-	 */
+	@Reference
 	RendererFactory rendererFactory;
+
+	@Reference
+	MailSessionFactory mailSessionFactory;
 
 
 	private static String SYSTEM_GRAPH_URI = "http://tpf.localhost/system.graph";
 	private UriRef systemGraphUri = new UriRef(SYSTEM_GRAPH_URI);
-
-	protected void activate(ComponentContext componentContext) {
-		properties = new Properties();
-
-		properties.setProperty("mail.debug", "false");
-
-		if (((String) componentContext.getProperties().get(MAIL_USE_TLS)).equals("true")) {
-			properties.setProperty("mail.smtp.starttls.enable", "true");
-			properties.setProperty("mail.smtp.auth", "true");
-			properties.setProperty("mail.smtp.socketFactory.fallback", "false");
-		}
-
-		properties.setProperty("mail.transport.protocol", "smtp");
-		properties.setProperty("mail.smtp.port", (String) componentContext.getProperties().get(MAIL_SMTP_PORT));
-		properties.setProperty("mail.smtp.host", (String) componentContext.getProperties().get(MAIL_SMTP_HOST));
-		properties.setProperty("smtpUser", (String) componentContext.getProperties().get(MAIL_SMTP_USER));
-		properties.setProperty("smtpPassword", (String) componentContext.getProperties().get(MAIL_SMTP_PASSWORD));
-	}
 
 	@Override
 	public void sendEmailToUser(final String fromUser, final String toUser,
@@ -206,7 +155,6 @@ public class MailManImpl implements MailMan {
 		}
 	}
 
-
 	@Override
 	public void sendEmailToUsers(final String fromUser, final String[] toUsers,
 			final String subject, final GraphNode graphNode,
@@ -256,36 +204,35 @@ public class MailManImpl implements MailMan {
 		sendEmail(from, to, cc, bcc, subject, message, "text/plain", null, null);
 	}
 
-	public void sendEmail(final InternetAddress from,final InternetAddress to,
-			final InternetAddress[] cc,	final InternetAddress[] bcc,
-			final String subject, final Object content,	final String mediaType,
-			final List<MediaType> acceptableMediaTypes,	final String mode)
+	@Override
+    public void sendEmail(InternetAddress from, InternetAddress to,
+            InternetAddress[] cc, InternetAddress[] bcc, String subject,
+            String message, String mediaType) throws MessagingException {
+        AccessController.checkPermission(
+                new MailManPermission("", MailManPermission.SEND_MAIL));
+        sendEmail(from, to, cc, bcc, subject, message, mediaType, null, null);
+    }
+
+	private void sendEmail(final InternetAddress from, final InternetAddress to,
+			final InternetAddress[] cc, final InternetAddress[] bcc,
+			final String subject, final Object content, final String mediaType,
+			final List<MediaType> acceptableMediaTypes, final String mode)
 			throws MessagingException {
+		final Session session = mailSessionFactory.getSession();
 		try {
 			AccessController.doPrivileged(new PrivilegedExceptionAction<Object>() {
 
 				@Override
 				public Object run() throws MessagingException {
-					Authenticator auth = new Authenticator() {
-
-						@Override
-						public PasswordAuthentication getPasswordAuthentication() {
-							return new PasswordAuthentication(properties.
-									getProperty("smtpUser"),
-									properties.getProperty("smtpPassword"));
-						}
-					};
-					Session session = Session.getInstance(properties, auth);
 					MimeMessage mimeMessage = new MimeMessage(session);
 					Date date = new Date();
 					mimeMessage.setSentDate(date);
 					mimeMessage.setSubject(subject);
 					if (content instanceof GraphNode) {
 						GraphNode graphNode = (GraphNode) content;
-						Renderer renderer = rendererFactory.
-							createRenderer(graphNode, mode, 
-							acceptableMediaTypes == null ? Collections.singletonList(
-							MediaType.WILDCARD_TYPE) : acceptableMediaTypes);
+						Renderer renderer = rendererFactory.createRenderer(graphNode, mode,
+								acceptableMediaTypes == null ? Collections.singletonList(
+								MediaType.WILDCARD_TYPE) : acceptableMediaTypes);
 						if (renderer == null) {
 							throw new MessagingException("No renderer appropriate found");
 						}
@@ -318,19 +265,21 @@ public class MailManImpl implements MailMan {
 
 	}
 
+
 	private InternetAddress getUserAddress(String user) throws MessagingException {
 		MGraph systemGraph = tcManager.getMGraph(systemGraphUri);
 
-		final String queryString = "SELECT ?email WHERE { " +
-				"?x " + FOAF.mbox + " ?email . " +
-				"?x " + PLATFORM.userName + " \"" + user + "\" . " +
-				"}";
+		final String queryString = "SELECT ?email WHERE { "
+				+ "?x " + FOAF.mbox + " ?email . "
+				+ "?x " + PLATFORM.userName + " \"" + user + "\" . "
+				+ "}";
 		try {
 			SelectQuery selectQuery = (SelectQuery) QueryParser.getInstance().parse(queryString);
 			ResultSet result = tcManager.executeSparqlQuery(selectQuery, systemGraph);
 			if (result.hasNext()) {
 				Resource email = result.next().get("email");
 				String emailString = ((UriRef) email).getUnicodeString();
+				//TODO should add personal name (if available) as well
 				return new InternetAddress(emailString.substring("mailto:".length()));
 			}
 		} catch (ParseException ex) {
