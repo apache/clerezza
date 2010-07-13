25a41d3ff37f80c655f5e92125c073ea380bffc2
Reto Bachmann-Gmür <reto@apache.org>
2010-07-13 20:52:01 +0000

    CLEREZZA-259: introduced PermissionParser utility class
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk@963858 13f79535-47bb-0310-9956-ffa450edef68

---
+++ org.apache.clerezza.parent/org.apache.clerezza.utils/src/main/java/org/apache/clerezza/utils/security/PermissionParser.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.clerezza.utils.security;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.security.Permission;
+
+
+import org.osgi.service.permissionadmin.PermissionInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author reto
+ */
+public class PermissionParser {
+
+	final static Logger logger = LoggerFactory.getLogger(PermissionParser.class);
+
+	public static Permission getPermission(String permissionDescription, ClassLoader classLoader) {
+		PermissionInfo permissionInfo = new PermissionInfo(
+					permissionDescription);
+
+		try {
+			Class clazz = classLoader.loadClass(permissionInfo.getType());
+			Constructor<?> constructor = clazz.getConstructor(
+					String.class, String.class);
+			return (Permission) constructor.newInstance(
+					permissionInfo.getName(), permissionInfo.getActions());
+		} catch (InstantiationException ie) {
+			logger.warn("{}", ie);
+			throw new RuntimeException(ie);
+		} catch (ClassNotFoundException cnfe) {
+			logger.warn("{}", cnfe);
+			throw new RuntimeException(cnfe);
+		} catch (NoSuchMethodException nsme) {
+			logger.warn("{}", nsme);
+			throw new RuntimeException(nsme);
+		} catch (InvocationTargetException ite) {
+			logger.warn("{}", ite);
+			throw new RuntimeException(ite);
+		} catch (IllegalAccessException iae) {
+			logger.warn("{}", iae);
+			throw new RuntimeException(iae);
+		}
+	}
+}
