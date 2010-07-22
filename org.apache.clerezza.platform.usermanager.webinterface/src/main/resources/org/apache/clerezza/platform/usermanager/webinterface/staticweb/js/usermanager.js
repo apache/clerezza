4a2ca32b8314d5b402e18e128f9deb919271a8e8
Tsuyoshi Ito <ito@apache.org>
2010-07-22 12:37:59 +0000

    add customfield to user updated to support IE
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk/org.apache.clerezza.parent@966623 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.clerezza.platform.usermanager.webinterface/src/main/resources/org/apache/clerezza/platform/usermanager/webinterface/staticweb/js/usermanager.js
+++ org.apache.clerezza.platform.usermanager.webinterface/src/main/resources/org/apache/clerezza/platform/usermanager/webinterface/staticweb/js/usermanager.js
@@ -1,10 +1,17 @@
 function refreshCustomInformation(user){
 	multipleValues = $("#userRoles").val() || []
+	var options = new AjaxOptions("get-customfields", "retrieving custom fields", function(data) {
+			$("#custominfos").html(data);
+		});
+	options.type = "GET";
+	options.url = "./custom-user";
+	options.dataType = "html";
 	if(user != null){
-		$("#custominfos").load("./custom-user?user="+user+"+&resource=" + document.location.href +"&roles="+multipleValues.join(",")+"&mode=naked");
+		options.data = "user="+user+"+&resource=" + document.location.href +"&roles="+multipleValues.join(",")+"&mode=naked"
 	} else {
-		$("#custominfos").load("./custom-user?resource=" + document.location.href +"&roles="+multipleValues.join(",")+"&mode=naked");
+		options.data = "resource=" + document.location.href +"&roles="+multipleValues.join(",")+"&mode=naked"
 	}
+	$.ajax(options);
 }
 
 function saveUser(){
