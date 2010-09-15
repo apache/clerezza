63ac12a5352609e0624ffa75f57b1a29316ecf5c
Reto Bachmann-Gmür <reto@apache.org>
2010-09-15 08:27:06 +0000

    moved BundlePathNode to utilities project
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk@997226 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.clerezza.parent/org.apache.clerezza.utils/src/main/java/org/apache/clerezza/utils/osgi/BundlePathNode.java
+++ org.apache.clerezza.parent/org.apache.clerezza.utils/src/main/java/org/apache/clerezza/utils/osgi/BundlePathNode.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.clerezza.web.fileserver;
+package org.apache.clerezza.utils.osgi;
 
 import java.io.IOException;
 import java.io.InputStream;
