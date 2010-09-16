19b55eb6ee1448542e9707496945c4726cee5446
Manuel Innerhofer <mir@apache.org>
2010-09-16 15:19:01 +0000

    LockableMGraphWrapperForTesting now only test if the relevant methods are locked
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk/org.apache.clerezza.parent@997785 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.clerezza.rdf.core.test/src/main/java/org/apache/clerezza/rdf/core/test/LockableMGraphWrapperForTesting.java
+++ org.apache.clerezza.rdf.core.test/src/main/java/org/apache/clerezza/rdf/core/test/LockableMGraphWrapperForTesting.java
@@ -18,20 +18,15 @@
  */
 package org.apache.clerezza.rdf.core.test;
 
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import org.apache.clerezza.rdf.core.Graph;
 import org.apache.clerezza.rdf.core.MGraph;
 import org.apache.clerezza.rdf.core.NonLiteral;
 import org.apache.clerezza.rdf.core.Resource;
 import org.apache.clerezza.rdf.core.Triple;
 import org.apache.clerezza.rdf.core.UriRef;
-import org.apache.clerezza.rdf.core.access.LockableMGraph;
-import org.apache.clerezza.rdf.core.event.FilterTriple;
-import org.apache.clerezza.rdf.core.event.GraphListener;
+import org.apache.clerezza.rdf.core.access.LockableMGraphWrapper;
 
 /**
  * Wrappes an MGraph as a LockableMGraph. If a method is called that reads
@@ -40,11 +35,10 @@ import org.apache.clerezza.rdf.core.event.GraphListener;
  *
  * @author rbn, mir
  */
-public class LockableMGraphWrapperForTesting implements LockableMGraph {
+public class LockableMGraphWrapperForTesting extends LockableMGraphWrapper {
 
-	private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+	private final ReentrantReadWriteLock lock = (ReentrantReadWriteLock) getLock();
 	private final Lock readLock = lock.readLock();
-	private final Lock writeLock = lock.writeLock();
 	private final MGraph wrapped;
 
 	/**
@@ -53,23 +47,8 @@ public class LockableMGraphWrapperForTesting implements LockableMGraph {
 	 * @param providedMGraph a non-lockable mgraph
 	 */
 	public LockableMGraphWrapperForTesting(final MGraph providedMGraph) {
+		super(providedMGraph);
 		this.wrapped = providedMGraph;
-	}
-
-	@Override
-	public ReadWriteLock getLock() {
-		return lock;
-	}
-
-	@Override
-	public Graph getGraph() {
-		LockChecker.checkIfReadLocked(lock);
-		readLock.lock();
-		try {
-			return wrapped.getGraph();
-		} finally {
-			readLock.unlock();
-		}
 	}
 
 	@Override
@@ -84,40 +63,6 @@ public class LockableMGraphWrapperForTesting implements LockableMGraph {
 	}
 
 	@Override
-	public int size() {
-		LockChecker.checkIfReadLocked(lock);
-		readLock.lock();
-		try {
-			return wrapped.size();
-		} finally {
-			readLock.unlock();
-		}
-	}
-
-	@Override
-	public boolean isEmpty() {
-		LockChecker.checkIfReadLocked(lock);
-		readLock.lock();
-		try {
-			return wrapped.isEmpty();
-		} finally {
-			readLock.unlock();
-		}
-	}
-
-	@Override
-	@SuppressWarnings("element-type-mismatch")
-	public boolean contains(Object o) {
-		LockChecker.checkIfReadLocked(lock);
-		readLock.lock();
-		try {
-			return wrapped.contains(o);
-		} finally {
-			readLock.unlock();
-		}
-	}
-
-	@Override
 	public Iterator<Triple> iterator() {
 		LockChecker.checkIfReadLocked(lock);
 		readLock.lock();
@@ -126,120 +71,6 @@ public class LockableMGraphWrapperForTesting implements LockableMGraph {
 		} finally {
 			readLock.unlock();
 		}
-	}
-
-	@Override
-	public Object[] toArray() {
-		LockChecker.checkIfReadLocked(lock);
-		readLock.lock();
-		try {
-			return wrapped.toArray();
-		} finally {
-			readLock.unlock();
-		}
-	}
-
-	@Override
-	public <T> T[] toArray(T[] a) {
-		LockChecker.checkIfReadLocked(lock);
-		readLock.lock();
-		try {
-			return wrapped.toArray(a);
-		} finally {
-			readLock.unlock();
-		}
-	}
-
-	@Override
-	public boolean containsAll(Collection<?> c) {
-		LockChecker.checkIfReadLocked(lock);
-		readLock.lock();
-		try {
-			return wrapped.containsAll(c);
-		} finally {
-			readLock.unlock();
-		}
-	}
-
-	@Override
-	public boolean add(Triple e) {
-		LockChecker.checkIfWriteLocked(lock);
-		writeLock.lock();
-		try {
-			return wrapped.add(e);
-		} finally {
-			writeLock.unlock();
-		}
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		LockChecker.checkIfWriteLocked(lock);
-		writeLock.lock();
-		try {
-			return wrapped.remove(o);
-		} finally {
-			writeLock.unlock();
-		}
-	}
-
-	@Override
-	public boolean addAll(Collection<? extends Triple> c) {
-		LockChecker.checkIfWriteLocked(lock);
-		writeLock.lock();
-		try {
-			return wrapped.addAll(c);
-		} finally {
-			writeLock.unlock();
-		}
-	}
-
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		LockChecker.checkIfWriteLocked(lock);
-		writeLock.lock();
-		try {
-			return wrapped.removeAll(c);
-		} finally {
-			writeLock.unlock();
-		}
-	}
-
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		LockChecker.checkIfWriteLocked(lock);
-		writeLock.lock();
-		try {
-			return wrapped.retainAll(c);
-		} finally {
-			writeLock.unlock();
-		}
-	}
-
-	@Override
-	public void clear() {
-		LockChecker.checkIfWriteLocked(lock);
-		writeLock.lock();
-		try {
-			wrapped.clear();
-		} finally {
-			writeLock.unlock();
-		}
-	}
-
-	@Override
-	public void addGraphListener(GraphListener listener, FilterTriple filter, long delay) {
-		wrapped.addGraphListener(listener, filter, delay);
-	}
-
-	@Override
-	public void addGraphListener(GraphListener listener, FilterTriple filter) {
-		wrapped.addGraphListener(listener, filter);
-	}
-
-	@Override
-	public void removeGraphListener(GraphListener listener) {
-		wrapped.removeGraphListener(listener);
 	}
 
 }
