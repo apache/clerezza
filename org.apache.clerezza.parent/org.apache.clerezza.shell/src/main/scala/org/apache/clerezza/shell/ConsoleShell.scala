995db3b99613f33915319d87090532540de9bcf6
Reto Bachmann-Gmür <reto@apache.org>
2010-11-06 10:19:15 +0000

    CLEREZZA-353: cleaned imports a bit
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk@1031991 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.clerezza.parent/org.apache.clerezza.shell/src/main/scala/org/apache/clerezza/shell/ConsoleShell.scala
+++ org.apache.clerezza.parent/org.apache.clerezza.shell/src/main/scala/org/apache/clerezza/shell/ConsoleShell.scala
@@ -20,36 +20,13 @@ package org.apache.clerezza.shell;
 
 
 
-import org.apache.felix.scr.annotations.Component;
 import org.osgi.framework.BundleContext
-import org.osgi.framework.BundleEvent
-import org.osgi.framework.BundleListener
 import org.osgi.service.component.ComponentContext;
 import org.osgi.framework.Bundle
-import java.io.{File, PrintWriter, Reader, StringWriter}
-import java.lang.reflect.InvocationTargetException
 import java.net._
-import java.security.PrivilegedActionException
-import java.security.AccessController
-import java.security.PrivilegedAction
-import java.util.{ArrayList, Arrays};
-import javax.script.ScriptContext
-import javax.script.{ScriptEngineFactory => JavaxEngineFactory, Compilable, 
-					 CompiledScript, ScriptEngine, AbstractScriptEngine, Bindings,
-					 SimpleBindings, ScriptException}
-//import scala.collection.immutable.Map
-import scala.actors.DaemonActor
 import scala.tools.nsc._;
 import scala.tools.nsc.interpreter._;
-import scala.tools.nsc.io.{AbstractFile, PlainFile, VirtualDirectory}
 import scala.tools.nsc.util._
-import scala.tools.nsc.symtab.SymbolLoaders
-import scala.tools.nsc.reporters.ConsoleReporter
-import scala.tools.nsc.reporters.ConsoleReporter
-import scala.tools.nsc.reporters.ConsoleReporter
-import scala.tools.nsc.reporters.Reporter
-import scala.tools.util.PathResolver
-import scala.tools.nsc.util.{ClassPath, JavaClassPath}
 import scala.actors.Actor
 import scala.actors.Actor._
 
