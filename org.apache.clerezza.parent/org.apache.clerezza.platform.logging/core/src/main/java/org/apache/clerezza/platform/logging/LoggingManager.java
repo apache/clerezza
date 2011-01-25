addf532b0068aade773b48ac5a1d4c86b28523e2
Tsuyoshi Ito <ito@apache.org>
2011-01-25 10:19:53 +0000

    CLEREZZA-393: add permission to access logging manager
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk@1063206 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.clerezza.parent/org.apache.clerezza.platform.logging/core/src/main/java/org/apache/clerezza/platform/logging/LoggingManager.java
+++ org.apache.clerezza.parent/org.apache.clerezza.platform.logging/core/src/main/java/org/apache/clerezza/platform/logging/LoggingManager.java
@@ -16,16 +16,16 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.clerezza.platform.logging;
 
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.net.URISyntaxException;
+import java.security.AccessControlException;
+import java.security.AccessController;
 import java.util.Dictionary;
 import java.util.Enumeration;
-import java.util.Hashtable;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
@@ -42,7 +42,6 @@ import org.apache.clerezza.jaxrs.utils.TrailingSlash;
 import org.apache.clerezza.platform.dashboard.GlobalMenuItem;
 import org.apache.clerezza.platform.dashboard.GlobalMenuItemsProvider;
 import org.apache.clerezza.platform.logging.ontologies.LOGGING;
-import org.apache.clerezza.platform.graphprovider.content.ContentGraphProvider;
 import org.apache.clerezza.platform.typerendering.RenderletManager;
 import org.apache.clerezza.platform.typerendering.scalaserverpages.ScalaServerPagesRenderlet;
 import org.apache.clerezza.rdf.core.BNode;
@@ -61,56 +60,57 @@ import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.ComponentContext;
 
-
-
 @Component
 @Services({
-	@Service(value=Object.class),
-	@Service(value=GlobalMenuItemsProvider.class)
+	@Service(value = Object.class),
+	@Service(value = GlobalMenuItemsProvider.class)
 })
-@Property(name="javax.ws.rs", boolValue=true)
+@Property(name = "javax.ws.rs", boolValue = true)
 @Path("admin/logging")
 public class LoggingManager implements GlobalMenuItemsProvider {
 
 	@Reference
 	private RenderletManager renderletManager;
-
 	@Reference
 	private ConfigurationAdmin configurationAdmin;
-
 	private String paxLoggingLocation = null;
-	public void activate(ComponentContext context) throws URISyntaxException, IOException {
+
+	public void activate(ComponentContext context)
+			throws URISyntaxException, IOException {
 		renderletManager.registerRenderlet(ScalaServerPagesRenderlet.class.getName(),
 				new UriRef(getClass().getResource(
 				"config-page-naked.ssp").toURI().toString()),
 				LOGGING.LoggingConfigPage, "naked",
 				MediaType.APPLICATION_XHTML_XML_TYPE, true);
-		
+
 		for (Bundle bundle : context.getBundleContext().getBundles()) {
 			if (bundle.getSymbolicName().equals("org.ops4j.pax.logging.pax-logging-service")) {
 				paxLoggingLocation = bundle.getLocation();
 				break;
 			}
 		}
-		
+
 	}
 
-	private void setProperties(Dictionary props) throws IOException {
+	private void setProperties(Dictionary props)
+			throws IOException {
 		Configuration config = getServiceConfig();
 		config.update(props);
 	}
 
 	/*private void setProperties(Hashtable properties) {
-		setProperties
+	setProperties
 	}*/
-
-	private Configuration getServiceConfig() throws IOException {
+	private Configuration getServiceConfig()
+			throws IOException {
 		Configuration config = configurationAdmin.getConfiguration("org.ops4j.pax.logging", paxLoggingLocation);
 		return config;
 	}
 
 	@GET
-	public GraphNode entry(@Context UriInfo uriInfo) throws IOException {
+	public GraphNode entry(@Context UriInfo uriInfo)
+			throws IOException {
+		AccessController.checkPermission(new LoggingManagerAccessPermission());
 		TrailingSlash.enforcePresent(uriInfo);
 		SimpleMGraph resultMGraph = new SimpleMGraph();
 		GraphNode result = new GraphNode(new BNode(), resultMGraph);
@@ -120,7 +120,8 @@ public class LoggingManager implements GlobalMenuItemsProvider {
 		return result;
 	}
 
-	private String getPropertiesAsString() throws IOException {
+	private String getPropertiesAsString()
+			throws IOException {
 		Configuration config = getServiceConfig();
 		Properties properties = new Properties();
 		Dictionary propertyDictionary = config.getProperties();
@@ -134,11 +135,12 @@ public class LoggingManager implements GlobalMenuItemsProvider {
 		return stringWriter.toString();
 	}
 
-
 	@POST
 	@Path("setConfiguration")
 	public Response setConfiguration(@Context UriInfo uriInfo,
-			@FormParam("configuration") String configuration) throws IOException {
+			@FormParam("configuration") String configuration)
+			throws IOException {
+		AccessController.checkPermission(new LoggingManagerAccessPermission());
 		Properties properties = new Properties();
 		properties.load(new StringReader(configuration));
 		setProperties(properties);
@@ -146,18 +148,16 @@ public class LoggingManager implements GlobalMenuItemsProvider {
 	}
 
 	@Override
-    public Set<GlobalMenuItem> getMenuItems() {
-        Set<GlobalMenuItem> items = new HashSet<GlobalMenuItem>();
+	public Set<GlobalMenuItem> getMenuItems() {
+		Set<GlobalMenuItem> items = new HashSet<GlobalMenuItem>();
+		try {
+			AccessController.checkPermission(new LoggingManagerAccessPermission());
+		} catch (AccessControlException e) {
+			return items;
+		}
+		items.add(new GlobalMenuItem("/admin/logging", "Logging", "Logging", 3,
+				"Administration"));
+		return items;
 
-        items.add(new GlobalMenuItem("/admin/logging", "Logging", "Logging", 3,
-                "Administration"));
-
-
-        return items;
-
-
-    }
-
-
-
+	}
 }
