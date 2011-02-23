8e9626479505c95ad918a716027f91f11ca630a6
Hasan Hasan <hasan@apache.org>
2011-02-23 01:44:46 +0000

    CLEREZZA-435: updated for coding convention compliance
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk@1073576 13f79535-47bb-0310-9956-ffa450edef68

--- parent/rdf.rdfjson/src/test/java/org/apache/clerezza/rdf/rdfjson/parser/RdfJsonParserProviderTest.java
+++ parent/rdf.rdfjson/src/test/java/org/apache/clerezza/rdf/rdfjson/parser/RdfJsonParserProviderTest.java
@@ -1,20 +1,18 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.clerezza.rdf.rdfjson.parser;
 
@@ -23,21 +21,16 @@ import java.util.Iterator;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.apache.clerezza.rdf.core.Graph;
 import org.apache.clerezza.rdf.core.MGraph;
 import org.apache.clerezza.rdf.core.Triple;
 import org.apache.clerezza.rdf.core.UriRef;
 import org.apache.clerezza.rdf.core.impl.SimpleMGraph;
 import org.apache.clerezza.rdf.core.serializedform.ParsingProvider;
 
-
-
-
 /**
  * @author tio
  */
 public class RdfJsonParserProviderTest {
-
 
 	@Test
 	public void testParser() {
@@ -47,7 +40,7 @@ public class RdfJsonParserProviderTest {
 		provider.parse(deserializedMGraph, jsonIn, "application/rdf+json", null);
 		Assert.assertEquals(deserializedMGraph.size(), 6);
 		Iterator<Triple> triples = deserializedMGraph.filter(new UriRef("http://base/child1"), null, null);
-		while(triples.hasNext()) {
+		while (triples.hasNext()) {
 			UriRef uri = triples.next().getPredicate();
 			Assert.assertEquals(uri.getUnicodeString(), "http://base/propertyB");
 		}
