69664aaf73368e6cf3d4ec226f24f2928756f91b
Reto Bachmann-Gmür <reto@apache.org>
2011-03-18 00:06:54 +0000

    CLEREZZA-465: An initial version, supporting creating skeletons with zz>Dev create 'scala_with_activator in /path/to/some/dir, the actual project prototype has yet to be finished and the naming skeleton/prototype has to made more consistent
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk/parent@1082762 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sourcebundle/src/main/resources/org/apache/clerezza/sourcebundle/skeletons/scala_with_activator/src/main/scala/Activator.scala
@@ -0,0 +1,43 @@
+package skeleton
+
+import org.osgi.framework.{BundleActivator, BundleContext, ServiceRegistration}
+import scala.collection.JavaConversions.asJavaDictionary
+import org.apache.clerezza.platform.typerendering.{Renderlet, RenderletManager}
+
+/**
+ * Activator for a bundle using Apache Clerezza.
+ */
+class Activator extends BundleActivator {
+
+	var helloWorldRegistration: ServiceRegistration = null
+	var renderletRegistration: ServiceRegistration = null
+	/**
+	 * called when the bundle is started, this method initializes the provided service
+	 */
+	def start(context: BundleContext) {
+		//import org.apache.clerezza.shell.OsgiDsl
+		//val osgiDsl = new OsgiDsl(context, System.out)
+		//import osgiDsl._
+		println("activating...")
+		val args = scala.collection.mutable.Map("javax.ws.rs" -> true)
+		helloWorldRegistration = context.registerService(classOf[Object].getName,
+												  new HelloWorld(), args)
+		val renderlet = new HelloWorldMessageRenderlet
+		val serviceReference = context.getServiceReference(classOf[RenderletManager].getName)
+		renderlet bindRenderletManager context.getService(serviceReference).asInstanceOf[RenderletManager]
+
+		renderletRegistration = context.registerService(classOf[Renderlet].getName,
+												  renderlet, null)
+		println("enjoy!")
+	}
+
+	/**
+	 * called when the bundle is stopped, this method unregisters the provided service
+	 */
+	def stop(context: BundleContext) {
+		helloWorldRegistration.unregister()
+		renderletRegistration.unregister()
+		println("bye")
+	}
+
+}
---
+++ sourcebundle/src/main/resources/org/apache/clerezza/sourcebundle/skeletons/scala_with_activator/src/main/scala/Ontology.scala
@@ -0,0 +1,12 @@
+package skeleton
+
+import org.apache.clerezza.rdf.core.UriRef
+
+object Ontology {
+
+	/**
+	 * The uri for the RDF type of HelloWordMessage
+	 */
+	val HelloWordMessageType = new UriRef("http://example.org/skeleton#HelloWordMessage")
+
+}
