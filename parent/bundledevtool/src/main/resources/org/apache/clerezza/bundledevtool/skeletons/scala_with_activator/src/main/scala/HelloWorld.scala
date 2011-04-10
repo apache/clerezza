3feeeba713ba0a9080a43888b261375a2f84000f
Reto Bachmann-Gmür <reto@apache.org>
2011-04-10 14:35:22 +0000

    improved skeleton project
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk@1090808 13f79535-47bb-0310-9956-ffa450edef68

--- parent/bundledevtool/src/main/resources/org/apache/clerezza/bundledevtool/skeletons/scala_with_activator/src/main/scala/HelloWorld.scala
+++ parent/bundledevtool/src/main/resources/org/apache/clerezza/bundledevtool/skeletons/scala_with_activator/src/main/scala/HelloWorld.scala
@@ -4,18 +4,24 @@ import javax.ws.rs._
 import org.apache.clerezza.rdf.core.BNode
 import org.apache.clerezza.rdf.core.impl.SimpleMGraph
 import org.apache.clerezza.rdf.ontologies.{DC, RDF}
-import org.apache.clerezza.rdf.utils.GraphNode;
+import org.apache.clerezza.rdf.utils.GraphNode
+import org.osgi.framework.BundleContext
+import org.apache.clerezza.osgi.services.ServicesDsl
+import org.apache.clerezza.platform.graphprovider.content.ContentGraphProvider
 
 /**
  * The classical Hello World root resource
  */
 @Path("hello-world")
-class HelloWorld {
+class HelloWorld(context: BundleContext) {
+	val servicesDsl = new ServicesDsl(context)
+	import servicesDsl._
 	@GET def get() = {
 		val resultMGraph = new SimpleMGraph();
 		val graphNode = new GraphNode(new BNode(), resultMGraph);
 		graphNode.addProperty(RDF.`type` , Ontology.HelloWordMessageType);
-		graphNode.addPropertyValue(DC.description,"Hello world");
+		val cgp: ContentGraphProvider = $[ContentGraphProvider]
+		graphNode.addPropertyValue(DC.description,"Hello world of "+cgp.getContentGraph.size);
 		graphNode;
 
 	}
