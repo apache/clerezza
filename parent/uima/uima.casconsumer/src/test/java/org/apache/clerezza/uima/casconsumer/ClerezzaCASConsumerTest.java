97935a92c4375ecf21a434f7a96bbe2bb24bcbb5
Tommaso Teofili <tommaso@apache.org>
2011-05-23 21:33:41 +0000

    [CLEREZZA-497] - added a simple test which demonstrates execution of the CAS Consumer actually creates an MGraph
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk@1126763 13f79535-47bb-0310-9956-ffa450edef68

--- parent/uima/uima.casconsumer/src/test/java/org/apache/clerezza/uima/casconsumer/ClerezzaCASConsumerTest.java
+++ parent/uima/uima.casconsumer/src/test/java/org/apache/clerezza/uima/casconsumer/ClerezzaCASConsumerTest.java
@@ -18,9 +18,23 @@
  */
 package org.apache.clerezza.uima.casconsumer;
 
+import org.apache.clerezza.rdf.core.MGraph;
+import org.apache.clerezza.rdf.core.UriRef;
+import org.apache.clerezza.rdf.core.access.TcManager;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.cas.admin.CASFactory;
+import org.apache.uima.cas.admin.CASMgr;
+import org.apache.uima.cas.admin.FSIndexRepositoryMgr;
+import org.apache.uima.cas.admin.TypeSystemMgr;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.text.AnnotationFS;
+import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.test.junit_extension.AnnotatorTester;
+import org.apache.uima.util.CasCreationUtils;
 import org.junit.Test;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
 /**
@@ -36,5 +50,48 @@ public class ClerezzaCASConsumerTest {
       e.printStackTrace();
       fail(e.getLocalizedMessage());
     }
+  }
+
+  @Test
+  public void simpleRunningTest() {
+    try {
+      AnnotatorTester annotatorTester = new AnnotatorTester("src/test/resources/TestClerezzaCASConsumerDescriptor.xml");
+
+      /* create a mock CAS */
+      CAS cas = createCAS();
+
+      cas.setDocumentText("Clerezza is an Apache project");
+      cas.setDocumentLanguage("en");
+
+      AnnotationFS annotation = cas.createAnnotation(cas.getAnnotationType(), 0, 9);
+      cas.addFsToIndexes(annotation);
+
+      /* execute ClerezzaCASConsumer on the created CAS */
+      annotatorTester.performTest(cas);
+
+      MGraph createdGraph = TcManager.getInstance().getMGraph(new UriRef("mytest-clerezza-uima-graph"));
+      assertNotNull(createdGraph);
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail(e.getLocalizedMessage());
+    }
+  }
+
+  private CAS createCAS() throws ResourceInitializationException, CASException {
+    CASMgr casMgr0 = CASFactory.createCAS();
+    CASMgr casMgr = null;
+    CasCreationUtils.setupTypeSystem(casMgr0, null);
+
+    TypeSystemMgr tsa = casMgr0.getTypeSystemMgr();
+
+    ((CASImpl) casMgr0).commitTypeSystem();
+
+    casMgr = CASFactory.createCAS(tsa);
+
+    casMgr.initCASIndexes();
+    FSIndexRepositoryMgr irm = casMgr.getIndexRepositoryMgr();
+    irm.commit();
+
+    return casMgr.getCAS().getCurrentView();
   }
 }
