08f4a14f628f19f2bf2a2d16727a74c8d522003d
Tommaso Teofili <tommaso@apache.org>
2011-06-03 16:14:09 +0000

    [CLEREZZA-555] - added a test case for remote resource tagger
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk@1131088 13f79535-47bb-0310-9956-ffa450edef68

---
+++ parent/uima/uima.concept-tagging/src/test/java/org/apache/clerezza/uima/concept/UIMARemoteResourceTaggerTest.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.clerezza.uima.concept;
+
+import org.apache.clerezza.rdf.core.Graph;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+/**
+ * @author tommaso
+ * @version $Id$
+ */
+public class UIMARemoteResourceTaggerTest {
+
+  @Test
+  public void serviceExecutionTest() {
+    try {
+      UIMARemoteResourceTaggerService service = new UIMARemoteResourceTaggerService();
+      Graph graph = service.tagUri("http://incubator.apache.org/clerezza", "04490000a72fe7ec5cb3497f14e77f338c86f2fe");
+      assertNotNull(graph);
+    } catch (Exception e) {
+      fail(e.getLocalizedMessage());
+    }
+  }
+}
