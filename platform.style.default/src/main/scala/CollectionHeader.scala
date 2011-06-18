a96b3f12dd2afa49ff2465e2ea48de3b74ce9b52
Reto Bachmann-Gmür <reto@apache.org>
2011-06-18 14:59:07 +0000

    CLEREZZA-563: Better title for collections
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk/parent@1137198 13f79535-47bb-0310-9956-ffa450edef68

--- platform.style.default/src/main/scala/CollectionHeader.scala
+++ platform.style.default/src/main/scala/CollectionHeader.scala
@@ -1,6 +1,8 @@
 package org.apache.clerezza.platform.style.default
 
 import org.apache.clerezza.rdf.ontologies._
+import org.apache.clerezza.rdf.scala.utils.RichGraphNode
+import org.apache.clerezza.rdf.scala.utils.Preamble._
 
 /**
  * A Renderlet for the menu
@@ -8,5 +10,8 @@ import org.apache.clerezza.rdf.ontologies._
 class CollectionHeader extends HeadedPageRenderlet {
 
 	override val getRdfType = HIERARCHY.Collection
+
+	override def defaultTitle(res: RichGraphNode) = (res*) +  " (Collection)"
+
 
 }
