19c526ff46ef94c94b6ea844ff9a8d96d8348f3c
Henry Story <bblfish@apache.org>
2011-06-22 13:00:31 +0000

    CLEREZZA-510: filled in intial test suite, simplified EasyGraph and co, and added initial Lang (EzLang?) class.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk@1138445 13f79535-47bb-0310-9956-ffa450edef68

--- parent/rdf.scala.utils/src/test/scala/org/apache/clerezza/rdf/scala/utils/EasyGraphTest.scala
+++ parent/rdf.scala.utils/src/test/scala/org/apache/clerezza/rdf/scala/utils/EasyGraphTest.scala
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -19,62 +19,140 @@
 package org.apache.clerezza.rdf.scala.utils
 
 import org.apache.clerezza.rdf.utils._
-import org.apache.clerezza.rdf.core._
-import org.apache.clerezza.rdf.core.impl._
 import org.apache.clerezza.rdf.ontologies._
-import org.junit._
 import Preamble._
+import org.apache.clerezza.rdf.core._
+import impl.{TypedLiteralImpl, TripleImpl, SimpleMGraph, PlainLiteralImpl}
+import org.junit._
 
 class EasyGraphTest {
 
+	val bblfishModulus = """
+	9D ☮ 79 ☮ BF ☮ E2 ☮ F4 ☮ 98 ☮ BC ☮ 79 ☮ 6D ☮ AB ☮ 73 ☮ E2 ☮ 8B ☮ 39 ☮ 4D ☮ B5 26 ✜ 68 ✜ 49 ✜ EE ✜ 71 ✜ 87 ✜
+	06 ✜ 32 ✜ C9 ✜ 9F ✜ 3F ✜ 94 ✜ E5 ✜ CB ✜ 4D ✜ B5 12 ☮ 35 ☮ 13 ☮ 69 ☮ 60 ☮ 81 ☮ 58 ☮ 79 ☮ 66 ☮ F3 ☮ 79 ☮ 20 ☮
+	91 ☮ 6A ☮ 3F ☮ 42 5A ✜ F6 ✜ 54 ✜ 42 ✜ 88 ✜ B2 ✜ E9 ✜ 19 ✜ 4A ✜ 79 ✜ 87 ✜ 2E ✜ 62 ✜ 44 ✜ 2D ✜ 7C 06 ☽ 78 ☽ F8
+	☽ FD ☽ 52 ☽ 92 ☽ 6D ☽ CD ☽ D6 ☽ F3 ☽ 28 ☽ 6B ☽ 1F ☽ DB ☽ CB ☽ D3 F2 ☮ 08 ☮ 34 ☮ 72 ☮ A2 ☮ 12 ☮ 75 ☮ AE ☮ D1
+	☮ 09 ☮ 17 ☮ D0 ☮ 88 ☮ 4C ☮ 04 ☮ 8E 04 ☾ E5 ☾ BF ☾ D1 ☾ 41 ☾ 64 ☾ D1 ☾ F7 ☾ 89 ☾ 6D ☾ 8B ☾ B2 ☾ F2 ☾ 46 ☾ C0
+	☾ 56 87 ☮ 8D ☮ B8 ☮ 7C ☮ C6 ☮ FE ☮ E9 ☮ 61 ☮ 88 ☮ 08 ☮ 61 ☮ DD ☮ E3 ☮ B8 ☮ B5 ☮ 47 ♥
+	"""
 
-	private val tinyGraph: Graph = {
+	/** import some references in order to reduce dependencies */
+
+	final val hex: UriRef = new UriRef("http://www.w3.org/ns/auth/cert#hex")
+	final val identity: UriRef = new UriRef("http://www.w3.org/ns/auth/cert#identity")
+	final val RSAPublicKey: UriRef = new UriRef("http://www.w3.org/ns/auth/rsa#RSAPublicKey")
+	final val modulus: UriRef = new UriRef("http://www.w3.org/ns/auth/rsa#modulus")
+	final val public_exponent: UriRef = new UriRef("http://www.w3.org/ns/auth/rsa#public_exponent")
+
+
+		private val tinyGraph: Graph = {
 		val gr = new SimpleMGraph
-		val subj= new BNode()
-		gr.add(new TripleImpl(subj,RDF.`type`, FOAF.Person))
-		gr.add(new TripleImpl(subj,FOAF.knows, new UriRef("http://bblfish.net/#hjs")))
-		gr.add(new TripleImpl(subj,FOAF.name, new PlainLiteralImpl("Henry Story", new Language("en"))))
-		val other = new BNode()
-		gr.add(new TripleImpl(subj,FOAF.knows, other))
-		gr.add(new TripleImpl(subj,FOAF.name,new PlainLiteralImpl("Danny Ayers")))
+		val reto= new BNode()
+		val danny = new BNode()
+		val henry = new UriRef("http://bblfish.net/#hjs")
+
+		gr.add(new TripleImpl(reto,RDF.`type`, FOAF.Person))
+		gr.add(new TripleImpl(reto,FOAF.name, new PlainLiteralImpl("Reto Bachman-Gmür", new Language("rm"))))
+		gr.add(new TripleImpl(reto,FOAF.title, new PlainLiteralImpl("Mr")))
+		gr.add(new TripleImpl(reto,FOAF.currentProject, new UriRef("http://clerezza.org/")))
+		gr.add(new TripleImpl(reto,FOAF.knows, henry))
+		gr.add(new TripleImpl(reto,FOAF.knows, danny))
+
+		gr.add(new TripleImpl(danny,FOAF.name,new PlainLiteralImpl("Danny Ayers", new Language("en"))))
+		gr.add(new TripleImpl(danny,RDF.`type`, FOAF.Person))
+		gr.add(new TripleImpl(danny,FOAF.knows, henry))
+		gr.add(new TripleImpl(danny,FOAF.knows, reto))
+
+		gr.add(new TripleImpl(henry,FOAF.name,new PlainLiteralImpl("Henry Story")))
+		gr.add(new TripleImpl(henry,RDF.`type`, FOAF.Person))
+		gr.add(new TripleImpl(henry,FOAF.knows, danny))
+		gr.add(new TripleImpl(henry,FOAF.knows, reto))
+
+		val pk = new BNode()
+		gr.add(new TripleImpl(pk,RDF.`type`,RSAPublicKey))
+		gr.add(new TripleImpl(pk,identity,henry))
+		gr.add(new TripleImpl(pk,modulus,LiteralFactory.getInstance().createTypedLiteral(65537)))
+		gr.add(new TripleImpl(pk,public_exponent,new TypedLiteralImpl(bblfishModulus,hex)))
 		gr.getGraph
 	}
 
 	@Test
 	def plainChracter {
-		/*val simpleMGraph = new SimpleMGraph
+/*
 		val g = new EasyGraph(simpleMGraph)
 		val sub = g.bnode
 		( g.u("http://bblfish.net/#hjs") a FOAF.Person
 		 has FOAF.name to {"Henry Story"}
 		)
 		
-		Assert.assertEquals(tinyGraph, simpleMGraph.getGraph)*/
+		Assert.assertEquals(tinyGraph, simpleMGraph.getGraph)
+*/
 	}
 
 	@Test
-	def usingArrows {
-
-		/*
+	def usingSymbolicArrows {
+		import org.apache.clerezza.rdf.scala.utils.EasyGraph._
+		import org.apache.clerezza.rdf.scala.utils.Lang._
+		 val ez = new EasyGraph()
 		 // example using arrows
 		 (
-		 sub ∈ FOAF.Person
-		 ⟝ FOAF.knows ⟶  "http://bblfish.net/#hjs".uri
-		 ⟝ FOAF.name ⟶ "Henry Story"(en)
-		 ⟝ FOAF.title ⟶ "Software"+" Architect"
-		 ⟝ FOAF.knows ⟶ ( g.bnode ⟝ FOAF.name ⟶ "Danny Ayers" )
+		   ez.bnode("reto") ∈ FOAF.Person
+			 ⟝ FOAF.name ⟶ "Reto Bachman-Gmür".lang(rm)
+			 ⟝ FOAF.title ⟶ "Mr"
+			 ⟝ FOAF.currentProject ⟶ "http://clerezza.org/".uri
+			 ⟝ FOAF.knows ⟶ (ez.u("http://bblfish.net/#hjs") ∈ FOAF.Person
+			          ⟝ FOAF.name ⟶ "Henry Story"
+			          ⟝ FOAF.currentProject ⟶ "http://webid.info/".uri
+			          ⟵ identity ⟞ ( ez.bnode ∈ RSAPublicKey
+			                 ⟝ modulus ⟶ 65537
+			                 ⟝ public_exponent ⟶ (bblfishModulus^^hex) // brackets needed due to precedence
+			          )
+			          ⟝ FOAF.knows ⟶ ez.bnode("reto")
+		 			    ⟝ FOAF.knows ⟶ ez.bnode("danny")
+			 )
+			 ⟝ FOAF.knows ⟶ (ez.bnode("danny") ∈ FOAF.Person
+			          ⟝ FOAF.name ⟶ "Danny Ayers"
+		             ⟝ FOAF.knows ⟶ "http://bblfish.net/#hjs".uri //knows
+					    ⟝ FOAF.knows ⟶ ez.bnode("reto")
+			 )
 		 )
-		 */
+
+		Assert.assertEquals("Both graphs should contain exactly the same triples",ez, tinyGraph)
+		ez.bnode("danny") ⟝  FOAF.name ⟶  "George"
+		Assert.assertNotSame("Added one more triple, so graphs should no longer be equal",ez, tinyGraph)
 	}
 
 	@Test
-	def usingBrackets {
-		/*
-		 // example using just brackets ( the apply() method )
-		 ( g.bnode(FOAF.knows)("http://bblfish.net/#hjs".uri,"http://farewellutopia.com/#me".uri)
-		 (FOAF.knows)(g.bnode(FOAF.name)("Danny Ayers"('en)))
+	def usingAsciiArrows {
+		import org.apache.clerezza.rdf.scala.utils.EasyGraph._
+		import org.apache.clerezza.rdf.scala.utils.Lang._
+		 val ez = new EasyGraph()
+		 // example using arrows
+		 (
+		   ez.bnode("reto").a(FOAF.Person)
+			 -- FOAF.name --> "Reto Bachman-Gmür".lang(rm)
+			 -- FOAF.title --> "Mr"
+			 -- FOAF.currentProject --> "http://clerezza.org/".uri
+			 -- FOAF.knows --> ( ez.u("http://bblfish.net/#hjs").a(FOAF.Person)
+			          -- FOAF.name --> "Henry Story"
+			          -- FOAF.currentProject --> "http://webid.info/".uri
+ 			          -<- identity -- (  ez.bnode.a(RSAPublicKey) //. notation because of precedence of operators
+			                       -- modulus --> 65537
+			                       -- public_exponent --> (bblfishModulus^^hex) // brackets needed due to precedence
+			                       )
+			          -- FOAF.knows --> ez.bnode("reto")
+		 			    -- FOAF.knows --> ez.bnode("danny")
+			 )
+			 -- FOAF.knows --> (ez.bnode("danny").a(FOAF.Person)
+			          -- FOAF.name --> "Danny Ayers"
+		             -- FOAF.knows --> "http://bblfish.net/#hjs".uri //knows
+					    -- FOAF.knows --> ez.bnode("reto")
+			 )
 		 )
-		 */
+		Assert.assertEquals("Both graphs should contain exactly the same triples",ez, tinyGraph)
+		ez.bnode("danny") -- FOAF.name --> "George"
+		Assert.assertNotSame("Added one more triple, so graphs should no longer be equal",ez, tinyGraph)
+
 	}
 
 
