fa2ad34a55dc7d08d66d0910bf9f39f4eed71157
Tommaso Teofili <tommaso@apache.org>
2012-07-18 22:23:21 +0000

    [CLEREZZA-695] - minor refactoring to uima.metadata-generator tests
    git-svn-id: https://svn.apache.org/repos/asf/incubator/clerezza/trunk@1363156 13f79535-47bb-0310-9956-ffa450edef68

--- uima/uima.metadata-generator/src/test/java/org/apache/clerezza/uima/metadatagenerator/UIMABaseMetadataGeneratorTest.java
+++ uima/uima.metadata-generator/src/test/java/org/apache/clerezza/uima/metadatagenerator/UIMABaseMetadataGeneratorTest.java
@@ -29,8 +29,6 @@ import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.fail;
-
 /**
  * Testcase for {@link UIMABaseMetadataGenerator}
  */
@@ -38,48 +36,32 @@ public class UIMABaseMetadataGeneratorTest {
   private static final String TEXT_TO_ANALYZE = "Italy, the defending champions and four-time World Cup winners, suffer a shock World Cup defeat to Slovakia, who win a remarkable game 3-2 to book their place in the last 16";
 
   @Test
-  public void testConstructor() {
-    try {
-      new UIMABaseMetadataGenerator();
-    }
-    catch (Exception e) {
-      fail(e.getLocalizedMessage());
-    }
+  public void testConstructor() throws Exception {
+    new UIMABaseMetadataGenerator();
   }
 
   @Test
-  public void testGenerateMethodWithUnsupportedMediaType() {
-    try {
-      UIMABaseMetadataGenerator baseMetadataGenerator = new UIMABaseMetadataGenerator();
-      String textToAnalyze = TEXT_TO_ANALYZE;
-      MGraph mGraph = new SimpleMGraph();
-      GraphNode node = new GraphNode(new UriRef("test"), mGraph);
-      MediaType wrongMediaType = MediaType.valueOf("multipart/form-data; boundary=AaB03x");
-      baseMetadataGenerator.generate(node, textToAnalyze.getBytes(), wrongMediaType);
-    } catch (Exception e) {
-      fail(e.getLocalizedMessage());
-    }
-
+  public void testGenerateMethodWithUnsupportedMediaType() throws Exception {
+    UIMABaseMetadataGenerator baseMetadataGenerator = new UIMABaseMetadataGenerator();
+    String textToAnalyze = TEXT_TO_ANALYZE;
+    MGraph mGraph = new SimpleMGraph();
+    GraphNode node = new GraphNode(new UriRef("test"), mGraph);
+    MediaType wrongMediaType = MediaType.valueOf("multipart/form-data; boundary=AaB03x");
+    baseMetadataGenerator.generate(node, textToAnalyze.getBytes(), wrongMediaType);
   }
 
   @Test
-  public void testGenerateMethodWithsupportedMediaType() {
-    try {
-
-      ExternalServicesFacade externalServicesFacade = new ExternalServicesFacade();
-      Map<String, Object> parameterSettings = new HashMap<String, Object>();
-      parameterSettings.put("apikey", "04490000a72fe7ec5cb3497f14e77f338c86f2fe");
-      parameterSettings.put("licenseID", "g6h9zamsdtwhb93nc247ecrs");
-      externalServicesFacade.setParameterSetting(parameterSettings);
-      UIMABaseMetadataGenerator baseMetadataGenerator = new UIMABaseMetadataGenerator(externalServicesFacade);
-      String textToAnalyze = TEXT_TO_ANALYZE;
-      MGraph mGraph = new SimpleMGraph();
-      GraphNode node = new GraphNode(new UriRef("test"), mGraph);
-      baseMetadataGenerator.generate(node, textToAnalyze.getBytes(), MediaType.TEXT_PLAIN_TYPE);
-    } catch (Exception e) {
-      fail(e.getLocalizedMessage());
-    }
-
+  public void testGenerateMethodWithsupportedMediaType() throws Exception {
+    ExternalServicesFacade externalServicesFacade = new ExternalServicesFacade();
+    Map<String, Object> parameterSettings = new HashMap<String, Object>();
+    parameterSettings.put("apikey", "04490000a72fe7ec5cb3497f14e77f338c86f2fe");
+    parameterSettings.put("licenseID", "g6h9zamsdtwhb93nc247ecrs");
+    externalServicesFacade.setParameterSetting(parameterSettings);
+    UIMABaseMetadataGenerator baseMetadataGenerator = new UIMABaseMetadataGenerator(externalServicesFacade);
+    String textToAnalyze = TEXT_TO_ANALYZE;
+    MGraph mGraph = new SimpleMGraph();
+    GraphNode node = new GraphNode(new UriRef("test"), mGraph);
+    baseMetadataGenerator.generate(node, textToAnalyze.getBytes(), MediaType.TEXT_PLAIN_TYPE);
   }
 
 }
