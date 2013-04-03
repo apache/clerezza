2de91ae0f13b3299a4b1604d1fdc6475269b0301
Reto Bachmann-Gmür <reto@apache.org>
2013-04-03 19:16:54 +0000

    CLEREZZA-741: adapted path instruction producing zip
    git-svn-id: https://svn.apache.org/repos/asf/clerezza/trunk@1464164 13f79535-47bb-0310-9956-ffa450edef68

--- site/deploy.sh
+++ site/deploy.sh
@@ -1,5 +1,5 @@
 svn co https://svn.apache.org/repos/asf/clerezza/site/production/ /tmp/site-production
-curl -u admin:admin "http://localhost:8080/admin/offline/download?baseUri=http://localhost:8080/&targetUri=http://incubator.apache.org/clerezza/&formatExtension=xhtml&formatExtension=rdf&formatExtension=png&formatExtension=html&formatExtension=js&formatExtension=jpeg&formatExtension=css&rootLinkPrefix=/clerezza"  > /tmp/site-production.zip
+curl -u admin:admin "http://localhost:8080/admin/offline/download?baseUri=http://localhost:8080/&targetUri=http://clerezza.apache.org/&formatExtension=xhtml&formatExtension=rdf&formatExtension=png&formatExtension=html&formatExtension=js&formatExtension=jpeg&formatExtension=css"  > /tmp/site-production.zip
 unzip -o /tmp/site-production.zip -d /tmp/site-production
 cd /tmp/site-production 
 svn add * --force
