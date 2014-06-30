95328ac1e9f1f7f36ecebc36b37d1de55dcf792a
Reto Bachmann-Gmür <reto@apache.org>
2014-06-30 21:28:41 +0200

    CLEREZZA-926: removed modules not in release

--- site/deploy.sh
+++
@@ -1,9 +0,0 @@
-svn co https://svn.apache.org/repos/asf/clerezza/site/production/ /tmp/site-production
-curl -u admin:admin "http://localhost:8080/admin/offline/download?baseUri=http://localhost:8080/&targetUri=http://clerezza.apache.org/&formatExtension=xhtml&formatExtension=rdf&formatExtension=png&formatExtension=html&formatExtension=js&formatExtension=jpeg&formatExtension=css"  > /tmp/site-production.zip
-cd ..
-mvn javadoc:aggregate
-cp -r ./target/site/apidocs /tmp/site-production/
-unzip -o /tmp/site-production.zip -d /tmp/site-production
-cd /tmp/site-production 
-svn add * --force
-svn commit -m "Auto commit by site deploy script" .
