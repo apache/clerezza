a04aec6eeb9bc7aaafc35a75002a0477da17e5f1
Reto Gmür <reto@apache.org>
2014-08-28 11:17:19 +0200

    CLEREZZA-948: avoid unnecessary merging of a set wit itself

--- rdf.utils/src/main/java/org/apache/clerezza/rdf/utils/smushing/SameAsSmusher.java
+++ rdf.utils/src/main/java/org/apache/clerezza/rdf/utils/smushing/SameAsSmusher.java
@@ -81,7 +81,7 @@ public class SameAsSmusher extends BaseSmusher {
                 }
             } else {
                 Set<NonLiteral> objectSet = node2EquivalenceSet.get(object);
-                if (objectSet != null) {
+                if ((objectSet != null) && (objectSet != equivalentNodes)) {
                     //merge two sets
                     for (NonLiteral res : objectSet) {
                         node2EquivalenceSet.remove(res);
