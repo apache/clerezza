c3676000e7874cc3285c01f1b7b10c4c369be12d
Reto Gmür <reto@apache.org>
2015-09-05 12:01:53 +0200

    renamed package

--- jaxrs.whiteboard.jersey/src/main/java/org/apache/clerezza/jaxrs/whiteboard/jersey/JerseyEndpoint.java
+++ jaxrs.whiteboard.jersey/src/main/java/org/apache/clerezza/jaxrs/whiteboard/jersey/JerseyEndpoint.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.stanbol.commons.web.base.jersey;
+package org.apache.clerezza.jaxrs.whiteboard.jersey;
 
 import java.io.IOException;
 import java.util.ArrayList;
